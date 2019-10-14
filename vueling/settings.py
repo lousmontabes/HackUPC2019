6e9a4b3fc5c1919efb69002b14ced762611f0ee9
Lluís Montabes <lluismontabes@gmail.com>
2019-10-14 12:57:44 +0200

    Added STATIC_ROOT

--- vueling/settings.py
+++ vueling/settings.py
@@ -121,6 +121,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
